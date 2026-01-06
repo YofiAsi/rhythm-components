75c8a0d67d1ceb1b045e78aac12888c7a464edd6
YofiAsi <asafshilo997@gmail.com>
2026-01-06 19:54:40 +0200

    removed test

--- addons/rhythm_components/test/rhythm_game.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-@onready var rhythm_animation_player: RhythmAnimationPlayer = $RhythmAnimationPlayer
-
-func _ready() -> void:
-	rhythm_animation_player.play_synced("new_animation", -1.0, 1.0, true)
